e6a4d9ac42775752abf40c396833f23f973eabe4
Gustavo Goedert <gusgoe@gmail.com>
2020-01-01 01:30:25 +0000

    bugfix and clarify hgr colors

--- Engine/Code/Include/LuxColor.h
+++ Engine/Code/Include/LuxColor.h
@@ -1,24 +1,32 @@
 #ifndef LUX_COLOR_H
 #define LUX_COLOR_H
 
-// color
 enum {
-    black,
-    darkBlue,
-    darkGreen,
-    blue,
-    brown,
-    gray,
-    green,
-    aqua,
-    magenta,
-    violet,
-    grey,
-    lightBlue,
-    orange,
-    pink,
-    yellow,
-    white
+    dhgr_black,     //hgr_black
+    dhgr_darkBlue,  //hgr_black
+    dhgr_darkGreen, //hgr_black
+    dhgr_blue,      //hgr_blue
+    dhgr_brown,     //hgr_black
+    dhgr_gray,      //hgr_blue
+    dhgr_green,     //hgr_green
+    dhgr_aqua,      //hgr_white
+    dhgr_magenta,   //hgr_black
+    dhgr_purple,    //hgr_purple
+    dhgr_grey,      //hgr_blue
+    dhgr_lightBlue, //hgr_white
+    dhgr_orange,    //hgr_orange
+    dhgr_pink,      //hgr_white
+    dhgr_yellow,    //hgr_white
+    dhgr_white      //hgr_white
+};
+
+enum {
+    hgr_black  = dhgr_black,
+    hgr_blue   = dhgr_blue,
+    hgr_green  = dhgr_green,
+    hgr_purple = dhgr_purple,
+    hgr_orange = dhgr_orange,
+    hgr_white  = dhgr_white
 };
 
 #endif
